e7e714fe0cba2ec1cf9440e06e743a12cd3305ad
LilianeAquino <lilianelou@hotmail.com>
2018-07-25 20:13:41 -0300

    Configurando o store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import StuffList from './StuffList';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      return (
+        <div className="app">
+          <StuffList />
+        </div>
+      );
+    }
 }
-
 export default App;
